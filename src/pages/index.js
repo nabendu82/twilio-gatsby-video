2c81150bde3c8e51ec384db4293fe11b7718b66b
nabendu82 <nabendu.biswas@gmail.com>
2020-04-04 00:09:28 +0530

    App v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,61 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState, useRef, useEffect } from "react"
+import TwilioVideo from "twilio-video"
+import StartForm from "../components/start-form"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const Video = ({ token }) => {
+  const localVidRef = useRef()
+  const remoteVidRef = useRef()
+
+  useEffect(() => {
+    TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
+      room => {
+        // Attach the local video
+        TwilioVideo.createLocalVideoTrack().then(track => {
+          localVidRef.current.appendChild(track.attach())
+        })
+
+        const addParticipant = participant => {
+          console.log("new participant!")
+          console.log(participant)
+          participant.tracks.forEach(publication => {
+            if (publication.isSubscribed) {
+              const track = publication.track
+
+              remoteVidRef.current.appendChild(track.attach())
+              console.log("attached to remote video")
+            }
+          })
+
+          participant.on("trackSubscribed", track => {
+            console.log("track subscribed")
+            remoteVidRef.current.appendChild(track.attach())
+          })
+        }
+
+        room.participants.forEach(addParticipant)
+        room.on("participantConnected", addParticipant)
+      }
+    )
+  }, [token])
+
+  return (
+    <div>
+      <div ref={localVidRef} />
+      <div ref={remoteVidRef} />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  )
+}
+
+const IndexPage = () => {
+  const [token, setToken] = useState(false);
+  return (
+    <Layout>
+    <SEO title="Home" />
+    {!token ? <StartForm storeToken={setToken} /> : <Video token={token} />}
   </Layout>
-)
+  )
+}
 
 export default IndexPage
